16d509eef947de74e9e0736ef95b8670ab2cf73a
deewebgr <119700877+deewebgr@users.noreply.github.com>
2022-12-23 19:05:16 +0200

    made AI more human friendly by changing params

--- server/server.js
+++ server/server.js
@@ -26,13 +26,12 @@ app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
         const response = await openai.createCompletion({
-
             model:"text-davinci-003",
             prompt: `${prompt}`,
             temperature:0,
-            max_tokens:3000,
+            max_tokens:100,
             top_p:1,
-            frequency_penalty:0.5,
+            frequency_penalty:0,
             presence_penalty:0,
         })
         res.status(200).send({
